c70534b870a522d2b502d1bbc70c0e8bcbafa03d
chenk <chenk@apical.com.cn>
2021-04-16 10:11:21 +0800

    优化编译脚本和编译选项

--- files/msc33x.toolchain.cmake
+++ files/msc33x.toolchain.cmake
@@ -4,3 +4,7 @@ set(CMAKE_SYSTEM_PROCESSOR armv7-a)
 
 set(CMAKE_C_COMPILER "arm-buildroot-linux-uclibcgnueabihf-gcc")
 set(CMAKE_CXX_COMPILER "arm-buildroot-linux-uclibcgnueabihf-g++")
+
+set(MNN_FORBID_MULTI_THREAD ON)
+set(MNN_USE_THREAD_POOL OFF)
+
